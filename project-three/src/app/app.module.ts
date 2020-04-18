50681f25c87a3b6624d8d7e787c0644b04f067a3
stjeandan-swc <stjean.dan.dev@gmail.com>
2020-04-17 20:13:29 -0400

    added dropdown component

--- project-three/src/app/app.module.ts
+++ project-three/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DropDownComponent } from './drop-down/drop-down.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropDownComponent
   ],
   imports: [
     BrowserModule,
